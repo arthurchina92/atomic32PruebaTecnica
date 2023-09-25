090f46fd959e33740ca57ac0efd4f61940cce678
arthurchina92 <arthurchinaa@gmail.com>
2023-09-25 16:34:05 -0600

    Deleted unused imports

--- PruebaTecnica/App.tsx
+++ PruebaTecnica/App.tsx
@@ -1,11 +1,6 @@
 import 'react-native-gesture-handler';
 import React from 'react';
 import Router from './src/router';
-import LoginScreen from './src/screens/LoginScreen';
-import PhoneValidationScreen from './src/screens/PhoneValidationScreen';
-import Footer from './src/components/Footer';
-import HomeScreen from './src/screens/HomeScreen';
-import FinalScreen from './src/screens/FinalScreen';
 import {store} from './store/store';
 import {Provider} from 'react-redux';
 
